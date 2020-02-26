1bd622af268d4e543738b53387ff7e5244b8c8c4
arieazhari <iniarieazhari@gmail.com>
2020-02-26 23:26:36 +0700

    context

--- src/app/container/views/Product/index.tsx
+++ src/app/container/views/Product/index.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
-
+import React, { useContext, useEffect } from "react";
+import Context from '../Home/Controller';
 export default function Product(): JSX.Element {
+    const { state, dispatch } = useContext(Context)
+    console.log('state product', state);
     return (
         <React.Fragment>
             <h1>Product List</h1>
